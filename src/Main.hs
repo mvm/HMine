b7283115ae50d6065151718322427b9d7721e95f
Miguel Vicente Moure <mvicmoure@gmail.com>
2012-01-25 14:26:06 +0100

    Added main gameloop

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,74 @@
 module Main where
 
-import Graphics.Rendering.OpenGL (($=))
-import Graphics.Rendering.OpenGL as GL
-import Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLFW
 import Control.Monad
+
+type Time = Double
+data Universe = NewUniverse {
+        isRunning :: Bool -- datos, datos, datos
+        }
+
+bigBang :: Universe
+bigBang = NewUniverse {isRunning = True} -- initial state of the game
+
+update :: Universe -> Universe
+update univ = NewUniverse {isRunning = not $ isRunning univ}
+
+render :: Universe -> Time -> IO ()
+render _ _ = do
+        clearColor $= Color4 0 0 0 0
+        clear [ColorBuffer]
+        swapBuffers
+        sleep 1
+
+maxSkipTicks :: Int
+maxSkipTicks = 5
+
+updatesPerSecond :: Double
+updatesPerSecond = 25
+
+skipTicks :: Double
+skipTicks = 1000/updatesPerSecond
+
+updateLoop :: Universe -> Time -> Int -> IO (Universe,Time)
+updateLoop state nextTick loops = get time >>= (\now -> if
+        now > nextTick && loops < maxSkipTicks then do
+                updateLoop (update state) (nextTick + skipTicks) (loops + 1)
+              else
+                return (state, nextTick)
+                )
+
+gameLoop :: Universe -> Time -> IO Universe
+gameLoop universe nextTick = if isRunning universe then do
+        (newUniverse,timeElapsed) <- updateLoop universe nextTick 0
+        now <- get time
+        let dt = (now + timeElapsed - skipTicks)/timeElapsed in render newUniverse dt
+        gameLoop newUniverse timeElapsed
+       else
+        return universe
 
 main::IO()
 main = do
-        initialized <- GLFW.initialize
+        initialized <- initialize
         when (not initialized) $ fail "Could not initialize GLFW"
-        opened <- GLFW.openWindow (GL.Size 640 480) [GLFW.DisplayAlphaBits 8] GLFW.Window
+        opened <- openWindow (Size 640 480) [DisplayAlphaBits 24] Window
         when (not opened) $ fail "Could not open window"
-        GLFW.windowTitle $= "Hello World!"
+        windowTitle $= "Hello World!"
         
-        GLFW.windowSizeCallback $= resizeCallback
+        windowSizeCallback $= resizeCallback
+
+        shadeModel $= Smooth
         
-        gameloop
+        beginningOfTime <- get time
+        _ <- gameLoop bigBang beginningOfTime
         
-        GLFW.closeWindow
-        GLFW.terminate
+        closeWindow
+        terminate
 
 resizeCallback :: Size -> IO ()
 resizeCallback size = do
-        GL.viewport $= (Position 0 0, size)
-        GL.matrixMode $= Projection
-        GL.loadIdentity
-        GL.frustum (-1) 1 (-1) 1 1 100
-
-gameloop :: IO ()
-gameloop = do
-        GL.clearColor $= Color4 0 0 0 0
-        GL.clear [GL.ColorBuffer]
-        GLFW.swapBuffers
-        GLFW.sleep 1
+        viewport $= (Position 0 0, size)
+        matrixMode $= Projection
+        loadIdentity
+        frustum (-1) 1 (-1) 1 1 100
